ea14bfafc15f0f42e0001924d4df8e01b06b2aab
Emlyn O'Regan <emlynoregan@gmail.com>
2012-12-23 12:34:09 +1030

    main.py can now take two arguments: [sourcefilename] and [transformfilename].
    So this command works:
    
    ./main.py ./examples/twitterget.json ./examples/twitterurltransform2.botl
    
    transforms twitterget.json using the transform twitterurltransform2.botl

--- main.py
+++ main.py
@@ -2,46 +2,54 @@
 
 import bOTL
 import json
+import sys
+
 def Main():
     
-    levent = {
-        "key": "key1",
-        "type": "new user",
-        "dtOccured": 12345678,
-        "assertingprincipalkey": "key2",
-        "detail": {
-            "key": "key3",
-            "name": "Fred Bloggs"
-            },
-        "clientkey": "key4",
-        }
-    
-    lclient = {
-        "key": "key4",
-        "clientname": "thingco"
-        }
-    
-    lsource = {
-        "event": levent,
-        "client": lclient
-        }
-    
-    ltransform = {
-        "Type": "Person",
-        "LastUpdate": "ref=.event .dtOccured",
-        "Name": "ref=.event :name",
-        "Client": {
-            "key": "ref=.event .clientkey",
-            "name": "ref=:clientname"       
-            },
-        "NameDoneInAMoreComplexWay": {
-            "ref": ":detail",
-            "id": "eventdetail",
-            "transform": {
-                "NameAgain": "ref=!eventdetail :name"
+    if len(sys.argv) >= 3:
+        with open(sys.argv[1]) as fp:
+            lsource = json.loads(fp.read())
+        with open(sys.argv[2]) as fp:
+            ltransform = json.loads(fp.read())
+    else:
+        levent = {
+            "key": "key1",
+            "type": "new user",
+            "dtOccured": 12345678,
+            "assertingprincipalkey": "key2",
+            "detail": {
+                "key": "key3",
+                "name": "Fred Bloggs"
+                },
+            "clientkey": "key4",
+            }
+        
+        lclient = {
+            "key": "key4",
+            "clientname": "thingco"
+            }
+        
+        lsource = {
+            "event": levent,
+            "client": lclient
+            }
+        
+        ltransform = {
+            "Type": "Person",
+            "LastUpdate": "ref=.event .dtOccured",
+            "Name": "ref=.event :name",
+            "Client": {
+                "key": "ref=.event .clientkey",
+                "name": "ref=:clientname"       
+                },
+            "NameDoneInAMoreComplexWay": {
+                "ref": ":detail",
+                "id": "eventdetail",
+                "transform": {
+                    "NameAgain": "ref=!eventdetail :name"
+                    }
                 }
             }
-        }
 
     ltarget = bOTL.Transform(lsource, ltransform)
     ltargetJson = json.dumps(ltarget, indent=4)
